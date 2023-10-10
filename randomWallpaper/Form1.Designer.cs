9f966abc7c4e2066a6e6a3acc9fda67b9017a3a5
chewiethesmugg <chewantha@gmail.com>
2023-10-10 13:10:59 -0400

    Add project files.

---
+++ randomWallpaper/Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace randomWallpaper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getWallpaper = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // getWallpaper
+            // 
+            this.getWallpaper.Location = new System.Drawing.Point(98, 150);
+            this.getWallpaper.Name = "getWallpaper";
+            this.getWallpaper.Size = new System.Drawing.Size(89, 23);
+            this.getWallpaper.TabIndex = 0;
+            this.getWallpaper.Text = " ";
+            this.getWallpaper.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.getWallpaper);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getWallpaperButton;
+        private System.Windows.Forms.Button getWallpaper;
+    }
+}
+
---
+++ randomWallpaper/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace randomWallpaper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+    public class Driver
+    {
+        [STAThread]
+        static void Main() 
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            using (NotifyIcon icon = new NotifyIcon())
+            {
+                icon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(Application.ExecutablePath);
+                icon.ContextMenu = new ContextMenu(new MenuItem[] {
+                new MenuItem("ImageExtractor", (s, e) => {new Form1().Show();}),
+                new MenuItem("Exit", (s, e) => { Application.Exit(); }),
+            });
+                icon.Visible = true;
+
+                Application.Run();
+                icon.Visible = false;
+            }
+        }
+    }
+}
